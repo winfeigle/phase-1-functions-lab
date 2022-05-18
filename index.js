67a626a02b0da03a1bca16b3c87110106a8cb5b1
Win Feigle <win.feigle@gmail.com>
2022-05-17 20:33:16 -0600

    commit tree 9a716edbda1936ac01d007210eab01e878f9fbea

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(val) {
+    return Math.abs(42 - val);
+}
+
+const distanceFromHqInFeet = function(val){
+    return 264 * distanceFromHqInBlocks(val);
+}
+
+const distanceTravelledInFeet = (start, destination) => {
+    return 264 * (Math.abs(start - destination));
+}
+
+const calculatesFarePrice = (start, destination) => {
+    if(distanceTravelledInFeet(start, destination) < 400){
+        return 0;
+    } else if(distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) < 2000){
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    } else if(distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500){
+        return 25;
+    } else return 'cannot travel that far';
+}
